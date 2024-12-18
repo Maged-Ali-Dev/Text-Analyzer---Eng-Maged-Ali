c3a2c1f7dcea63691edcf94f3a376b763c61134b
Maged <Maged@DESKTOP-FBIRLAD>
2024-12-18 13:10:14 +0200

    Add project files.

---
+++ Text Analyzer - Eng Maged Ali/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Text_Analyzer___Eng_Maged_Ali
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtWordCount = new System.Windows.Forms.TextBox();
+            this.txtLetterCount = new System.Windows.Forms.TextBox();
+            this.txtSentenceCount = new System.Windows.Forms.TextBox();
+            this.txtCharacterCount = new System.Windows.Forms.TextBox();
+            this.txtMostFrequentWord = new System.Windows.Forms.TextBox();
+            this.txtAverageReadingTime = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtGrammarFeedback = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtWordCount
+            // 
+            this.txtWordCount.Location = new System.Drawing.Point(351, 217);
+            this.txtWordCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtWordCount.Name = "txtWordCount";
+            this.txtWordCount.ReadOnly = true;
+            this.txtWordCount.Size = new System.Drawing.Size(274, 24);
+            this.txtWordCount.TabIndex = 0;
+            this.txtWordCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtLetterCount
+            // 
+            this.txtLetterCount.Location = new System.Drawing.Point(351, 266);
+            this.txtLetterCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtLetterCount.Name = "txtLetterCount";
+            this.txtLetterCount.ReadOnly = true;
+            this.txtLetterCount.Size = new System.Drawing.Size(274, 24);
+            this.txtLetterCount.TabIndex = 3;
+            this.txtLetterCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtSentenceCount
+            // 
+            this.txtSentenceCount.Location = new System.Drawing.Point(351, 319);
+            this.txtSentenceCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSentenceCount.Name = "txtSentenceCount";
+            this.txtSentenceCount.ReadOnly = true;
+            this.txtSentenceCount.Size = new System.Drawing.Size(274, 24);
+            this.txtSentenceCount.TabIndex = 4;
+            this.txtSentenceCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtCharacterCount
+            // 
+            this.txtCharacterCount.Location = new System.Drawing.Point(351, 374);
+            this.txtCharacterCount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCharacterCount.Name = "txtCharacterCount";
+            this.txtCharacterCount.ReadOnly = true;
+            this.txtCharacterCount.Size = new System.Drawing.Size(274, 24);
+            this.txtCharacterCount.TabIndex = 5;
+            this.txtCharacterCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMostFrequentWord
+            // 
+            this.txtMostFrequentWord.Location = new System.Drawing.Point(351, 423);
+            this.txtMostFrequentWord.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtMostFrequentWord.Name = "txtMostFrequentWord";
+            this.txtMostFrequentWord.ReadOnly = true;
+            this.txtMostFrequentWord.Size = new System.Drawing.Size(274, 24);
+            this.txtMostFrequentWord.TabIndex = 6;
+            this.txtMostFrequentWord.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtAverageReadingTime
+            // 
+            this.txtAverageReadingTime.Location = new System.Drawing.Point(351, 469);
+            this.txtAverageReadingTime.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAverageReadingTime.Name = "txtAverageReadingTime";
+            this.txtAverageReadingTime.ReadOnly = true;
+            this.txtAverageReadingTime.Size = new System.Drawing.Size(274, 24);
+            this.txtAverageReadingTime.TabIndex = 7;
+            this.txtAverageReadingTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(351, 528);
+            this.btnAnalyze.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(274, 75);
+            this.btnAnalyze.TabIndex = 8;
+            this.btnAnalyze.Text = "Analyze";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label1.Location = new System.Drawing.Point(164, 270);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Letter Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label2.Location = new System.Drawing.Point(164, 220);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "WordCount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label3.Location = new System.Drawing.Point(164, 322);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Sentence Count";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label4.Location = new System.Drawing.Point(164, 378);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Character Count";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label5.Location = new System.Drawing.Point(164, 425);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Most Frequent Word";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.label6.Location = new System.Drawing.Point(164, 469);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Average Reading Time";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label7.Location = new System.Drawing.Point(794, 217);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(146, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Grammar Feedback";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(257, 18);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(495, 155);
+            this.txtInput.TabIndex = 16;
+            this.txtInput.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(102, 63);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(146, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Enter your text here...";
+            // 
+            // txtGrammarFeedback
+            // 
+            this.txtGrammarFeedback.Location = new System.Drawing.Point(754, 242);
+            this.txtGrammarFeedback.Name = "txtGrammarFeedback";
+            this.txtGrammarFeedback.Size = new System.Drawing.Size(218, 361);
+            this.txtGrammarFeedback.TabIndex = 18;
+            this.txtGrammarFeedback.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Text_Analyzer___Eng_Maged_Ali.Properties.Resources.Magnify_1x_0_7s_200px_200px;
+            this.pictureBox1.Location = new System.Drawing.Point(798, 340);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 107);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(996, 638);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtGrammarFeedback);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.txtAverageReadingTime);
+            this.Controls.Add(this.txtMostFrequentWord);
+            this.Controls.Add(this.txtCharacterCount);
+            this.Controls.Add(this.txtSentenceCount);
+            this.Controls.Add(this.txtLetterCount);
+            this.Controls.Add(this.txtWordCount);
+            this.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text Analyzer Eng Maged Ali";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWordCount;
+        private System.Windows.Forms.TextBox txtLetterCount;
+        private System.Windows.Forms.TextBox txtSentenceCount;
+        private System.Windows.Forms.TextBox txtCharacterCount;
+        private System.Windows.Forms.TextBox txtMostFrequentWord;
+        private System.Windows.Forms.TextBox txtAverageReadingTime;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox txtGrammarFeedback;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Text Analyzer - Eng Maged Ali/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NHunspell;
+
+namespace Text_Analyzer___Eng_Maged_Ali
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Attach event handlers for text changes and form load
+            txtInput.TextChanged += txtInput_TextChanged;
+            this.Load += Form1_Load; // Subscribe to the Load event
+        }
+
+        // Form Load event: Sets the active control to txtInput when the form loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtInput; // Explicitly set the active control to txtInput
+        }
+
+        // TextChanged event handler: Called when the text in txtInput changes
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            UpdateTextDirection(); // Update the text direction based on the input text
+        }
+
+        // Updates the text direction based on whether the text is Arabic or English
+        private void UpdateTextDirection()
+        {
+            // If the text contains Arabic characters, set RightToLeft to Yes
+            if (IsArabic(txtInput.Text))
+            {
+                txtInput.RightToLeft = RightToLeft.Yes;
+            }
+            else
+            {
+                // Otherwise, set RightToLeft to No (left-to-right for English)
+                txtInput.RightToLeft = RightToLeft.No;
+            }
+
+            // Update the alignment for the first word after changing the text direction
+            HandleFirstWordAlignment();
+        }
+
+        // Check if the text contains Arabic characters using regular expression
+        private bool IsArabic(string text)
+        {
+            return Regex.IsMatch(text, @"\p{IsArabic}");
+        }
+
+        // Check if the text contains English characters using regular expression
+        private bool IsEnglish(string text)
+        {
+            return Regex.IsMatch(text, @"[a-zA-Z]");
+        }
+
+        // Adjust alignment for the first word based on the detected language
+        private void HandleFirstWordAlignment()
+        {
+            // If the first word is Arabic, re-render the text
+            if (IsArabic(txtInput.Text) && !txtInput.Text.StartsWith(" "))
+            {
+                txtInput.SelectionStart = txtInput.Text.Length;
+                txtInput.ScrollToCaret();
+            }
+            // If the first word is English, ensure proper left alignment
+            else if (IsEnglish(txtInput.Text) && !txtInput.Text.StartsWith(" "))
+            {
+                txtInput.SelectionStart = txtInput.Text.Length;
+                txtInput.ScrollToCaret();
+            }
+        }
+
+        // Analyze the input text when the "Analyze" button is clicked
+        private async void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            txtGrammarFeedback.Text = "";
+            string inputText = txtInput.Text;
+
+            // Check if input text is empty or contains only whitespace
+            if (string.IsNullOrWhiteSpace(inputText))
+            {
+                MessageBox.Show("Please enter some text to analyze.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            btnAnalyze.Enabled = false;
+            pictureBox1.Visible = true;
+            await Task.Delay(5000);  // Delay for 5 seconds (you can change this value)
+
+            // Perform your analysis after the delay
+            
+            // Perform text analysis
+            int wordCount = GetWordCount(inputText);
+            int letterCount = GetLetterCount(inputText);
+            int sentenceCount = GetSentenceCount(inputText);
+            string mostFrequentWord = GetMostFrequentWord(inputText);
+            double averageReadingTime = GetAverageReadingTime(wordCount);
+            int characterCount = GetCharacterCount(inputText);
+            string grammarFeedback = CheckGrammar(inputText);
+
+            // Display results in respective text boxes
+            txtWordCount.Text = wordCount.ToString();
+            txtLetterCount.Text = letterCount.ToString();
+            txtSentenceCount.Text = sentenceCount.ToString();
+            txtMostFrequentWord.Text = mostFrequentWord;
+            txtAverageReadingTime.Text = averageReadingTime.ToString("0.00") + " minutes";
+            txtCharacterCount.Text = characterCount.ToString();
+            txtGrammarFeedback.Text = grammarFeedback;
+            
+            pictureBox1.Visible = false;
+            btnAnalyze.Enabled = true;
+        }
+
+        // Helper method to count words in the text
+        private int GetWordCount(string text)
+        {
+            return Regex.Matches(text, "\\b\\w+\\b").Count;
+        }
+
+        // Helper method to count the total number of characters in the text
+        private int GetCharacterCount(string text)
+        {
+            return text.Length;
+        }
+
+        // Helper method to count the total number of letters in the text (ignores numbers and symbols)
+        private int GetLetterCount(string text)
+        {
+            return text.Count(char.IsLetter);
+        }
+
+        // Helper method to count the number of sentences in the text
+        private int GetSentenceCount(string text)
+        {
+            return Regex.Matches(text, "[.!?](\\s|$)").Count;
+        }
+
+        // Calculates the average reading time based on the word count (200 words per minute average speed)
+        private double GetAverageReadingTime(int wordCount)
+        {
+            const double wordsPerMinute = 200.0; // Average reading speed
+            return wordCount / wordsPerMinute;
+        }
+
+        // Get the most frequent word in the text
+        private string GetMostFrequentWord(string text)
+        {
+            var words = Regex.Matches(text.ToLower(), "\\b\\w+\\b")
+                             .Cast<Match>()
+                             .Select(m => m.Value);
+
+            return words.GroupBy(w => w)
+                        .OrderByDescending(g => g.Count())
+                        .FirstOrDefault()?.Key ?? "N/A";
+        }
+
+        // Check grammar and spelling using Hunspell (detects language and suggests corrections)
+        private string CheckGrammar(string text)
+        {
+            // Determine if the text is Arabic or English
+            bool isArabic = IsArabic(text);
+
+            // Use the corresponding language dictionary files based on detected language
+            string affFile = isArabic ? "ar.aff" : "en_US.aff";
+            string dicFile = isArabic ? "ar.dic" : "en_US.dic";
+
+            // Check if dictionary files exist
+            if (!System.IO.File.Exists(affFile) || !System.IO.File.Exists(dicFile))
+            {
+                return "Dictionary files not found for the selected language.";
+            }
+
+            // Use Hunspell to check spelling
+            using (var hunspell = new Hunspell(affFile, dicFile))
+            {
+                var words = Regex.Matches(text, "\\b\\w+\\b")
+                                 .Cast<Match>()
+                                 .Select(m => m.Value)
+                                 .ToList();
+
+                string corrections = "";
+                var incorrectWords = new List<string>();
+
+                // Check each word for spelling errors
+                foreach (var word in words)
+                {
+                    if (!hunspell.Spell(word))
+                    {
+                        var suggestions = hunspell.Suggest(word);
+                        corrections += $"{word}: {string.Join(", ", suggestions)}\n";
+                        incorrectWords.Add(word);
+                    }
+                }
+
+                // Highlight incorrect words in the input text
+                HighlightIncorrectWords(text, incorrectWords);
+
+                return string.IsNullOrWhiteSpace(corrections)
+                    ? "No spelling errors detected."
+                    : corrections;
+            }
+        }
+
+        // Highlights incorrect words in the RichTextBox with red color
+        private void HighlightIncorrectWords(string text, List<string> incorrectWords)
+        {
+            // First, clear the text to remove any previous formatting
+            txtInput.Text = text;
+
+            // Track where we are in the text
+            int index = 0;
+
+            // Loop through the incorrect words and highlight them
+            foreach (var word in incorrectWords)
+            {
+                int wordStartIndex = text.IndexOf(word, index);
+
+                // Loop through all occurrences of the incorrect word
+                while (wordStartIndex >= 0)
+                {
+                    // Select the word in the RichTextBox
+                    txtInput.Select(wordStartIndex, word.Length);
+
+                    // Change the color to red for the selected word
+                    txtInput.SelectionColor = Color.Red;
+
+                    // Update the index to start searching after the current word
+                    index = wordStartIndex + word.Length;
+
+                    // Find the next occurrence of the same word
+                    wordStartIndex = text.IndexOf(word, index);
+                }
+            }
+
+            // Reset the text color to black after processing all incorrect words
+            txtInput.Select(0, 0);
+            txtInput.SelectionColor = Color.Black;
+        }
+    }
+}
